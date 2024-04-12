d2c19300991297dc14262fa76c5ecdbfd9d018b0
IZZYLOVE <izzylovu@gmail.com>
2024-04-12 12:03:44 +0100

    here

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -33,13 +33,13 @@ app.use(requestedAt) //middleware
 
 
 
-app.use(express.static(path.join(__dirname, 'dist'))); // Serve static files from the "public" directory (React build files).
+app.use(express.static(path.join(__dirname, 'build'))); // Serve static files from the "public" directory (React build files).
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))) // lets us access static files in the upload folder
 
 
-// Define a route to serve the "dist/index.html" file as the default route
+// Define a route to serve the "build/index.html" file as the default route
 app.get('/*', (req, res) => {
-res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 //   //DEFAULT ROUTE (for all other routes that are not matched)
